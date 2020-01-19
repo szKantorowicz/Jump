b7187b972fceb4b542f192ae3b5437fcec7765de
sz.kantorowicz <szkantorowicz@gmail.com>
2020-01-19 23:42:09 +0100

    stworzenie RepoBase

---
+++ Jumpings/Jumpings/Repos/BaseRepo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumpings.Repos
+{
+    public abstract class BaseRepo<T> where T: class, new()
+    {
+        public JumpingsContext Context { get; } = new JumpingsContext();
+        protected DbSet<T> Table;
+
+        internal int SaveChanges()
+        {
+            try
+            {
+                return Context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw;
+            }
+            catch (DbUpdateException ex)
+            {
+                throw;
+            }
+            catch (CommitFailedException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        internal async Task<int> SaveChangesAsync()
+        {
+            try
+            {
+                return await Context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                throw;
+            }
+            catch (DbUpdateException ex)
+            {
+                throw;
+            }
+            catch (CommitFailedException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+           
+        }
+    }
+
+}
