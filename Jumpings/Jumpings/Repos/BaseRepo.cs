ecb64463d7c1b55f44d54e12baf0ce8e48c44ccc
sz.kantorowicz <szkantorowicz@gmail.com>
2020-01-20 00:01:40 +0100

    dodanie Disposable oraz metod do BaseRepo

--- Jumpings/Jumpings/Repos/BaseRepo.cs
+++ Jumpings/Jumpings/Repos/BaseRepo.cs
@@ -8,10 +8,95 @@ using System.Threading.Tasks;
 
 namespace Jumpings.Repos
 {
-    public abstract class BaseRepo<T> where T: class, new()
+    public abstract class BaseRepo<T> : IDisposable where T : class, new()
     {
         public JumpingsContext Context { get; } = new JumpingsContext();
         protected DbSet<T> Table;
+
+        bool disposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+            if(disposing)
+            {
+                Context.Dispose();
+            }
+            disposed = true;
+        }
+
+        public int Add(T entity)
+        {
+            Table.Add(entity);
+            return SaveChanges();
+        }
+
+        public async Task<int> AddAsync(T entity)
+        {
+            Table.Add(entity);
+            return await SaveChangesAsync();
+        }
+
+        public int AddRange(IList<T> entities)
+        {
+            Table.AddRange(entities);
+            return SaveChanges();
+        }
+        public Task<int> AddRangeAsync(IList<T> entities)
+        {
+            Table.AddRange(entities);
+            return SaveChangesAsync();
+        }
+
+        public int AddRange(IEnumerable<T> entities)
+        {
+            Table.AddRange(entities);
+            return SaveChanges();
+        }
+
+        public Task<int> AddRangeAsync(IEnumerable<T> entities)
+        {
+            Table.AddRange(entities);
+            return SaveChangesAsync();
+        }
+
+        public int Save(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            return SaveChanges();
+        }
+
+        public async Task<int> SaveAsync(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            return await SaveChangesAsync();
+        }
+
+        public int Delete(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Deleted;
+            return SaveChanges();
+        }
+
+        public async Task<int> DeleteAsync(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Deleted;
+            return await SaveChangesAsync();
+        }
+
+        public List<T> ExecuteQuery(string sql) => Table.SqlQuery(sql).ToList();
+
+        public Task<List<T>> ExecuteQueryAsync(string sql)
+            => Table.SqlQuery(sql).ToListAsync();
+        public List<T> ExecuteQuery(string sql, object[] sqlParametersObjects)
+            => Table.SqlQuery(sql, sqlParametersObjects).ToList();
+        public Task<List<T>> ExecuteQueryAsync(string sql, object[] sqlParametersObjects)
+            => Table.SqlQuery(sql).ToListAsync();
 
         internal int SaveChanges()
         {
