bedea06fc7411080aeea6f82499188db94888e66
sz.kantorowicz <szkantorowicz@gmail.com>
2020-01-20 00:40:21 +0100

    przed dodawaniem skoczków

---
+++ Jumpings/Jumpings/Repos/DataInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumpings.Repos
+{
+    public class DataInitializer : DropCreateDatabaseAlways<JumpingsContext>
+    {
+        protected override void Seed (JumpingsContext context)
+        {
+            var jumper= new List<Jumper>
+            {
+                new Jumper{ID=1,FirstName=""}
+            }
+
+        }
+        
+    }
+}
---
+++ Jumpings/Jumpings/Repos/JumpRepo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Jumpings.Repos
+{
+    public class InventoryRepo : BaseRepo<Jumper>, IRepo<Jumper>
+    {
+        public InventoryRepo()
+        {
+            Table = Context.Jumper;
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Jumper> ExcuteQuery(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Jumper> ExcuteQuery(string sql, object[] sqlParametrsObjects)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Jumper>> ExcuteQueryAsync(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Jumper>> ExcuteQueryAsync(string sql, object[] sqlParametrsObjects)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Jumper> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Jumper>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Jumper GetOne(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Jumper> GetOneAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
