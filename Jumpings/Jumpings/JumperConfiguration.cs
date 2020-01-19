0733ef9526f7ed6891c56ac485cdbe9f5b9cf420
sz.kantorowicz <szkantorowicz@gmail.com>
2020-01-19 23:02:01 +0100

    dadanie bazy

---
+++ Jumpings/Jumpings/JumperConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Jumpings
+{
+    
+    public class JumperConfiguration : EntityTypeConfiguration<Jumper>
+    {
+        public JumperConfiguration()
+        {
+            HasKey(j => j.ID);
+
+            Property(j => j.ID)
+                    .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)
+                    .IsRequired();
+
+            Property(j => j.FirstName)
+                    .HasMaxLength(100)
+                    .IsRequired();
+
+            Property(j => j.LastName)
+                    .HasMaxLength(100)
+                    .IsRequired();
+
+            Property(j => j.Country)
+                    .HasMaxLength(100)
+                    .IsRequired();
+        }
+    }
+}
