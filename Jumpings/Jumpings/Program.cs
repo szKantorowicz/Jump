beeb141564081ca52e434b7e909c21129ba3be78
pkantorowicz <piotrkantorowicz@gmail.com>
2020-01-30 23:02:00 +0100

    Dodanie komentarzy z objasnieniem co trzeba zrobic do nastepnego spotkania.

--- Jumpings/Jumpings/Program.cs
+++ Jumpings/Jumpings/Program.cs
@@ -3,6 +3,7 @@ using NLog;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 
 namespace Jumpings
 {
@@ -17,28 +18,35 @@ namespace Jumpings
 
             var jumpers = GetAllJumpers(jumpingsContext);
 
-            foreach (Jumper jumper in jumpers)
-
+            foreach (var jumper in jumpers)
             {
                 Console.WriteLine(jumper.ToString());
-            }  
+            }
 
             Console.Read();
         }
 
         private static List<Jumper> GetAllJumpers(JumpingsContext jumpingsContext)
         {
+            // pobieram skoczkow 
             using (var repo = new JumperRepo(jumpingsContext))
             {
-                var jumpers = repo.GetAll();
+                var jumpers = repo.GetAll()
+                    .OrderBy(jumper => jumper.LastName)
+                    .ToList();
+
                 return jumpers;
             }
+            // pobieranie ukonczone
         }
         private static void DataInitialize(JumpingsContext jumpingsContext)
         {
             IDataInitializer dataInitializer = new DataInitializer();
             dataInitializer.InitializeData(jumpingsContext);
         }
+
+        // create jumper result message
+        // var message = new jumper result message
     }
 }
  
